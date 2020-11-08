f7bab2b06b160b07daccbec5ee4beae4c7c2b9a9
hcc-1993 <834587431@qq.com>
2020-11-08 11:23:07 +0800

    提交代码TestMain

---
+++ src/test/java/com/example/elasticsearch/repository/TestMain.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearch.repository;
+
+/**
+ * class_name:
+ * <br/>description:
+ * <br/>author: HCC
+ * <br/>create: 2020-11-08 11:13
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("333333");
+    }
+}
